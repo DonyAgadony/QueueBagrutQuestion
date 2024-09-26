54193dd172277e7e7127c6c52a0c751a19ea9aea
Daniel <donybof1@gmail.com>
2024-09-26 15:50:00 +0300

    כןמן

--- obj/Debug/net8.0/QueueBagrutQuestion.AssemblyInfo.cs
+++ obj/Debug/net8.0/QueueBagrutQuestion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueueBagrutQuestion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddfc6178b36de893e0ab9db04473d8753b6fe1e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueueBagrutQuestion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueueBagrutQuestion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
